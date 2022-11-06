da69cb30a12b2c78b23bc700fcbdf14611f4cbb0
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-11-05 23:04:02 -0600

    changed physics slightly

--- Soroban/GameScene.swift
+++ Soroban/GameScene.swift
@@ -12,7 +12,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
-    
+    var selectedNode:SKShapeNode?
+    var history:[TouchInfo]?
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
@@ -35,18 +36,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         print("contact")
         print(contact.bodyA);
         print(contact.bodyB);
-        
+        //let fixedJoint = SKPhysicsJointFixed.joint(withBodyA: contact.bodyA, bodyB: contact.bodyB, anchor: contact.contactPoint)
+        //self.physicsWorld.add(fixedJoint)
     }
     func touchDown(atPoint pos : CGPoint) {
-        for bead in beads{
-            if bead.contains(pos) && (!hasSelected){
-                bead.setSelected(b: true)
-                hasSelected = true
+        for bead in beads {
+            if bead.contains(pos){
+                bead.selected=true;
                 let move = SKAction.move(to:CGPoint(x: bead.position.x, y: pos.y), duration: 0.01)
                 bead.run(move);
             }
         }
-        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
@@ -68,15 +68,34 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for touch in touches {
+            self.touchDown(atPoint: touch.location(in: self))
+            let location = touch.location(in:self)
+            let node = self.atPoint(location)
+           
+                // Step 1
+            selectedNode = node as? SKShapeNode;
+                // Stop the sprite
+            selectedNode?.physicsBody?.velocity = CGVectorMake(0,0)
+                // Step 2: save information about the touch
+            history = [TouchInfo(location:location, time:touch.timestamp)]
+            
+        }
+       
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            self.touchMoved(toPoint: touch.location(in: self))
+           
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for t in touches { self.touchUp(atPoint: t.location(in: self))
+            
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -97,9 +116,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         // Update entities
         for entity in self.entities {
+ 
             entity.update(deltaTime: dt)
         }
         
         self.lastUpdateTime = currentTime
     }
+}
+
+struct TouchInfo {
+    var location:CGPoint
+    var time:TimeInterval
 }
